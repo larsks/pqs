df8f6630ec7a22c06a2c60d162f4bedf9258b9da
Lars Kellogg-Stedman <lars@oddbit.com>
2010-02-18 20:44:54 -0500

    prep for upload to pypi

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='gitblogger',
-        version='20100210.1',
+setup(name='pqs',
+        version='20100217.1',
         description='Parse strings containing quote-delimited tokens.',
         long_description=read('README.rst'),
         author='Lars Kellogg-Stedman',
