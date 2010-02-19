ce0b40248b72a63fbdcdfc49bef6f33c374b0b1b
Lars Kellogg-Stedman <lars@oddbit.com>
2010-02-18 21:46:26 -0500

    increment version for MANIFEST fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='pqs',
-        version='20100218.1',
+        version='20100218.2',
         description='Parse strings containing quote-delimited tokens.',
         long_description=read('README.rst'),
         author='Lars Kellogg-Stedman',
